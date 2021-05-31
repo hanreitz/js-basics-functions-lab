5357a2903deb7135139b62cfa6765cd4651cca82
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-31 15:35:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(inputLocation) {
+  return Math.abs(inputLocation - 42);
+}
+
+function distanceFromHqInFeet(inputLocation) {
+  return distanceFromHqInBlocks(inputLocation)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return 264*Math.abs(start - destination);
+}
+
+function calculatesFarePrice(start, destination) {
+  const tripDistance = distanceTravelledInFeet(start, destination);
+  
+  switch (true) {
+    case tripDistance < 400 :
+      return 0;
+    case tripDistance < 2000 :
+      return 0.02 * (tripDistance - 400);
+    case tripDistance > 2500 :
+      return 'cannot travel that far';
+    case tripDistance > 2000 :
+      return 25;
+  };
+}
